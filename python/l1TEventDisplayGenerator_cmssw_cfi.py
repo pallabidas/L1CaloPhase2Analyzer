415a6fd463cff1c1a420394c7b6a49144c1004db
Stephanie Kwan <skwan@princeton.edu>
2021-07-14 06:58:45 +0200

    Add separate python cfi for cmssw analyzer for clarity, improve README

---
+++ python/l1TEventDisplayGenerator_cmssw_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+
+l1NtupleProducer = cms.EDAnalyzer("L1TEventDisplayGenerator",
+                                  folderName              = cms.untracked.string("firstFolder"),
+                                  #packedPfCands           = cms.InputTag("packedPFCandidates"),
+                                  #pfCands                 = cms.InputTag("particleFlow"),
+                                  ecalDigis = cms.InputTag("simEcalEBTriggerPrimitiveDigis","","HLT"),
+                                  hcalDigis = cms.InputTag("simHcalTriggerPrimitiveDigis","","HLT"),
+                                  #clusters                = cms.InputTag('Phase2L1CaloEGammaEmulatorProducer')
+                                  clusters  = cms.InputTag('L1EGammaClusterEmuProducer')
+)
